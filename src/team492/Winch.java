d3e2db26b443c4b1a434677216f0a6c8a54e4e72
dazen16 <dzeng10@hotmail.com>
2017-02-04 14:12:45 -0800

    Winch methods

--- src/team492/Winch.java
+++ src/team492/Winch.java
@@ -2,10 +2,25 @@ package team492;
 
 import frclib.FrcPneumatic;
 import edu.wpi.first.wpilibj.DigitalInput;
+import trclib.TrcDigitalTrigger;
 
 public class Winch
 {
-    private FrcPneumatic winch, sensor; 
-        
+    private FrcPneumatic winchmotor, proxsensor;
     
+    
+    
+    public void safety(){
+        
+    }
+    
+    public void climb(){
+        
+    }
+    
+    public void close(){
+        if boolean proxsensor{
+            
+        }
+    }
 }
